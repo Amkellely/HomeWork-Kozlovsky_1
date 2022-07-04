6bda05ea1f3b6577322399ba988c35408ad13247
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-07-04 20:22:44 +0300

    HomeWork 1
    Kostya Kozlovsky

--- Main.java
+++ Main.java
@@ -1,100 +1,80 @@
-package Lesson2;
+package com.company;
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        //. Дано расстояние L в сантиметрах. Используя операцию деления
-        //нацело, найти количество полных метров в нем (1 метр = 100 см).
-       /* Scanner in=new Scanner(System.in);
-        System.out.println("Введите целое число- кол-во сантиметров");
-        int L=in.nextInt();
-        System.out.println(L/100+"метра");
-            */
-//Дана масса M в килограммах. Используя операцию деления нацело,
-//найти количество полных тонн в ней (1 тонна = 1000 кг).
-       /* Scanner in=new Scanner(System.in);
-        System.out.println("Введите целое число в килограммах");
-        int L=in.nextInt();
-        System.out.println(L/1000+"кг");
-          */
-        // Дан размер файла в байтах. Используя операцию деления нацело,
-        //найти количество полных килобайтов, которые занимает данный файл
-        //(1 килобайт = 1024 байта).
-        /*Scanner in= new Scanner(System.in);
-        System.out.println("Введите число байтов");
-        int bytes = in.nextInt();
-        System.out.println(bytes/1024+"килобайтов");
-         */
-        //Дано двузначное число. Вывести вначале его левую цифру (десятки), а затем — его правую цифру (единицы). Для нахождения десятков
-        //использовать операцию деления нацело, для нахождения единиц — операцию взятия остатка от деления.
-       /* Scanner in= new Scanner(System.in);
-        System.out.print("Введите двухзначное число");
-        int number = in.nextInt();
-        System.out.println("Десятки"+number /10);
-        System.out.println("Единицы"+number % 10);
-         */
-        //Дано двузначное число. Найти сумму и произведение его цифр
-       /* Scanner in= new Scanner(System.in);
-        System.out.println("Введите двузначное число ");
-        int M = in.nextInt();
-        int a= M/10;
-        int b= M % 10;
-        System.out.println((a+b)+ "Сумма");
-        System.out.println((a*b)+ "Произведение число");
-         */
-        // Дано двузначное число. Вывести число, полученное при перестановке цифр исходного числа.
-       /* Scanner in= new Scanner(System.in);
-        int M = in.nextInt();
-        int number_a= M/10;
-        int number_b= M % 10;
-        int x= number_b *10+  number_a;
-        System.out.println(x+ "Новое число");
-        */
-        //Дано трехзначное число. Используя одну операцию деления нацело,
-        //вывести первую цифру данного числа (сотни).
-        /* Scanner in= new Scanner(System.in);
-        System.out.println("Введите трехзначное число ");
-        int number = in.nextInt();
-        System.out.println(" Сотни "+ number / 100);
-       */
-        //Дано трехзначное число. Вывести вначале его последнюю цифру
-        //(единицы), а затем — его среднюю цифру (десятки).
-       /* Scanner in= new Scanner(System.in);
-        System.out.println("Введите трехзначное число ");
-        int number = in.nextInt();
-        System.out.println(" Единицы "+ number % 10);
-        int numbers = (number /10) %10;
-        System.out.println(" Десятки "+ numbers);
-                */
 
-        //Дано трехзначное число. Найти сумму и произведение его цифр.
-        /*Scanner in= new Scanner(System.in);
-        System.out.println("Введите трехзначное число ");
-        int number = in.nextInt();
-        int number_edinici= number%10;
-        int number_des=(number /10)% 10;
-        int number_sotni=number /100;
-        System.out.println(" Сумма " +(number_edinici+number_des+number_sotni));
-        System.out.println(" Произведение " +(number_edinici*number_des*number_sotni));
+    public static void main(String[] args) {
+        //Козловский Константин
+	//Begin13◦ Даны два круга с общим центром и радиусами R1 и R2 (R1 > R2).
+        //Найти площади этих кругов S1 и S2, а также площадь S3 кольца, внешний
+        //радиус которого равен R1, а внутренний радиус равен R2:
+        // S1 = π·(R1)^2,  S2 = π·(R2)^2
+        // S3 = S1 − S2.
+        //В качестве значения π использовать 3.14.
+         /* Scanner in = new Scanner(System.in);
+
+        System.out.print("r1=");
+        double r1 = in.nextDouble();
+        System.out.print("r2=");
+        double r2 = in.nextDouble();
+
+        double s1 = 3.14 * Math.pow(r1, 2);
+        System.out.println("s1=" + s1);
+        double s2 = 3.14 * Math.pow(r2, 2);
+        System.out.println("s2=" + s2);
+        double s3 = s1 - s2;
+        System.out.println("s3=" + s3);
         */
-        //Дано трехзначное число. Вывести число, полученное при прочтении исходного числа справа налево.
-       /* Scanner in= new Scanner(System.in);
-        System.out.println("Введите трехзначное число ");
-        int number = in.nextInt();
-        int number_edinici= number%10;
-        int number_des=(number /10)% 10;
-        int number_sotni=number /100;
-        int res=100*number_edinici + 10*number_des+number_sotni ;
-        System.out.println(" В обратном порядке  " +res);
-                */
-        //Дано трехзначное число. В нем зачеркнули первую слева цифру и
-        //приписали ее справа. Вывести полученное число.
-        Scanner in= new Scanner(System.in);
-        System.out.print("Введите трехзначное число: ");
-        int number = in.nextInt();
-        int number_sotni=number /100;
-        int ostatok= number % 100;
-        int res= ostatok*10+ number_sotni;
-        System.out.println(" Убрали сотник, приписали сотни слева  " +res);
-     }
+        //Begin16. Найти расстояние между двумя точками с заданными координатами x1 и x2 на числовой оси: |x2 − x1|.
+        /*Scanner in=new Scanner (System.in);
+        double x1=in.nextDouble();
+        double x2=in.nextDouble();
+        double D=Math.abs(x1-x2);
+        System.out.println("D = "+D);
+          */
+        //Begin19. Даны координаты двух противоположных вершин прямоугольника: (x1,y1),  (x2,y2). Стороны прямоугольника параллельны осям координат. Найти периметр и площадь данного прямоугольника.
+       /* Scanner in=new Scanner (System.in);
+       System.out.println("x1,y1");
+        double x1=in.nextDouble();
+        double y1=in.nextDouble();
+        System.out.println("x2,y2");
+        double x2=in.nextDouble();
+        double y2=in.nextDouble();
+        System.out.println("P = "+2*(y2-y1+x2-x1));
+        System.out.println("S = "+(y2-y1)*(x2-x1));
+         */
+//Begin20◦ Найти расстояние между двумя точками с заданными координатами
+//(x1, y1) и (x2, y2) на плоскости. Расстояние вычисляется по формуле
+        //q(x2 − x1)^2+ (y2 − y1)^2
+        /*Scanner in=new Scanner (System.in);
+        System.out.println("x1,y1");
+        double x1=in.nextDouble();
+        double y1=in.nextDouble();
+        System.out.println("x2,y2");
+        double x2=in.nextDouble();
+        double y2=in.nextDouble();
+        System.out.println("D = "+Math.sqrt(Math.pow((y2-y1),2)*Math.pow((x2-x1),2)));
+         */
+            //Begin21.Даны координаты трех вершин треугольника: (x1, y1), (x2, y2), (x3, y3).
+        //Найти его периметр и площадь, используя формулу для расстояния между двумя точками на плоскости (см. задание Begin20). Для нахождения
+        //площади треугольника со сторонами a, b, c использовать формулу Герона:
+        //S =√p·(p − a)·(p − b)·(p − c),
+        //где p = (a + b + c)/2 — полупериметр.
+        Scanner in=new Scanner (System.in);
+        System.out.println("x1,y1");
+        double x1=in.nextDouble();
+        double y1=in.nextDouble();
+        System.out.println("x2,y2");
+        double x2=in.nextDouble();
+        double y2=in.nextDouble();
+        System.out.println("x3,y3");
+        double x3=in.nextDouble();
+        double y3=in.nextDouble();
+        double a=Math.sqrt(Math.pow(x2-x1,2)+Math.pow((y2-y1),2));
+        double b=Math.sqrt(Math.pow(x3-x2,2)+Math.pow((y3-y2),2));
+        double c=Math.sqrt(Math.pow(x3-x1,2)+Math.pow((y3-y1),2));
+        double p=(a+b+c)/2;
+        double S=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+        System.out.println("S = "+S);
+        System.out.println("P = "+p*2);
+    }
 }
