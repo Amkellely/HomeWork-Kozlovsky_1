7413257af50c8310fe67c1535455ad6999919b28
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-07-06 20:00:24 +0300

    HomeWork 2

--- Main.java
+++ Main.java
@@ -6,15 +6,15 @@ public class Main {
         //Козловский Константин
 	// Integer15. Дано трехзначное число.
         // Вывести число, полученное при перестановке цифр сотен и десятков исходного числа (например, 123 перейдет в213).
-        /*Scanner in = new Scanner(System.in);
+        /* Scanner in = new Scanner(System.in);
         int a= in.nextInt();
         System.out.println("Введите трёхзначное число :");
             int res= a/100*10+ (a%100)/10*100+a%10 ;
         System.out.println("Число, полученное при перестановке цифр: "+res);
           */
-        // Integer15. Дано трехзначное число.
+        // Integer16. Дано трехзначное число.
         // Вывести число, полученное при перестановке цифр десятков и единиц исходного числа (например, 123 перейдетв 132).
-         /*Scanner in = new Scanner(System.in);
+         /* Scanner in = new Scanner(System.in);
         System.out.println("Введите трёхзначное число :");
         int a =in.nextInt();
         int res = a/100*100+ (a%100)/10+a%10*10;
