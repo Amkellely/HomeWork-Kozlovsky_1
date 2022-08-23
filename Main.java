fbf7bcb84f342eb3908bc8310b55f60075a12dcb
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-08-23 20:00:51 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,124 +1,34 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        //Козловский Константин
-       //Array 20. Дан массив размера N и целые числа K и L (1 ≤ K ≤ L ≤ N). Найти
-        //сумму элементов массива с номерами от K до L включительно
-       /* Scanner in = new Scanner(System.in);
-        int[] a = new int[10];
-        int n, k, l;
+        // Задан массив А, содержащий 12 целых случайных чисел от -5 до 5. Найти сумму элементов массива
+       /* Scanner scanner = new Scanner(System.in);
+        int [] array = new int[12];
+        for(int i=0; i< array.length; i++){
+            array[i] = (int) Math.round((Math.random()*10)-5);
+            System.out.println(array[i]);
+            int sum= array[i] + array[i];
+        }*/
 
-        System.out.print("N: ");
+        // Задан массив который содержит 13 случайных целых чисел от -12 до 12.  Найти произведение отрицательных элементов массива
+         /*Scanner in = new Scanner(System.in);
+        int n, array[], negative = 1;
+        System.out.print("Введите размер массива: ");
         n = in.nextInt();
-
-        System.out.print("K: ");
-        k = in.nextInt();
-
-        System.out.print("L: ");
-        l = in.nextInt();
-
-        for(int i = 0; i < n; ++i) {
-            System.out.print("a[" + (i + 1) + "] : ");
-            a[i] = in.nextInt();
+        int i =  in.nextInt();
+        array = new int [13];
+        for (int i=0 ; i < array.length ; i++){
+            array[i] = (int) ( Math.random() * 12+12);
+            for (int i: array)
+                System.out.print( i + "\n" );
+            for (int i: array)
+                if ( i < 0)
+                    negative *= i;
         }
-
-        int sum = 0;
-        for(int i = k - 1; i <= l - 1; ++i) {
-            sum += a[i];
-        }
-
-        System.out.print(sum + "\n: ");*/
-
-        //Array 21.Дан массив размера N и целые числа K и L (1 ≤ K ≤ L ≤ N).
-        //Найти среднее арифметическое элементов массива с номерами от K до L
-        //включительно.
-        /* Scanner in = new Scanner(System.in);
-        int[] a = new int[10];
-        int n, k, l;
-
-        System.out.print("N: ");
-        n = in.nextInt();
-
-        System.out.print("K: ");
-        k = in.nextInt();
-
-        System.out.print("L: ");
-        l = in.nextInt();
-
-        for(int i = 0; i < n; ++i) {
-            System.out.print("a[" + (i + 1) + "] : ");
-            a[i] = in.nextInt();
-        }
-
-        int sum = 0;
-        for(int i = k - 1; i <= l - 1; ++i) {
-            sum += a[i];
-        }
-
-        System.out.printf("%f\n: ", (float)sum / (float)(l - k + 1));*/
-
-        //Array 22.  Дан массив размера N и целые числа K и L (1 < K ≤ L ≤ N). Найти
-        //сумму всех элементов массива, кроме элементов с номерами от K до L
-        //включительно.
-        /*Scanner in = new Scanner(System.in);
-        int[] a = new int[10];
-        int n, k, l;
-
-        System.out.print("N: ");
-        n = in.nextInt();
-
-        System.out.print("K: ");
-        k = in.nextInt();
-
-        System.out.print("L: ");
-        l = in.nextInt();
-
-        for(int i = 0; i < n; ++i) {
-            System.out.print("a[" + i + "] : ");
-            a[i] = in.nextInt();
-        }
-
-        int sum = 0;
-        for(int i = 0; i < k - 1; ++i) {
-            sum += a[i];
-        }
-        for(int i = l; i < n; ++i) {
-            sum += a[i];
-        }
-        System.out.print(sum + "\n: ");*/
-
-        //Array 23. Дан массив размера N и целые числа K и L (1 < K ≤ L ≤ N).
-        //Найти среднее арифметическое всех элементов массива, кроме элементов
-        //с номерами от K до L включительно
-          /*Scanner in = new Scanner(System.in);
-        int[] a = new int[10];
-        int n, k, l;
-
-        System.out.print("N: ");
-        n = in.nextInt();
-
-        System.out.print("K: ");
-        k = in.nextInt();
-
-        System.out.print("L: ");
-        l = in.nextInt();
-
-        for(int i = 0; i < n; ++i) {
-            System.out.print("a[" + (i + 1) + "] : ");
-            a[i] = in.nextInt();
-        }
-
-        int sum = 0;
-        for(int i = 0; i < k - 1; ++i) {
-            sum += a[i];
-        }
-        for(int i = l; i < n; ++i) {
-            sum += a[i];
-        }
-        System.out.printf("%f\n: ", (float)sum / (float)(k - 1 + (n - l)));*/
-        }
-    }
+        System.out.println("Произведение отрицательных эл-тов массива = " + negative);*/
+                }
+           }
