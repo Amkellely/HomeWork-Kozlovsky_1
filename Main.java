13d0ab16ecaa49ad07f0983248a36e093b66b17a
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-09-08 21:25:31 +0300

    урок 14. Матрицы

--- Main.java
+++ Main.java
@@ -1,101 +1,153 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static void createDeck( ArrayList<Integer> fdeck){
-        for(int k=0; k<4; k++){
-            for (int i=1; i<=9; i++){
-           fdeck.add(i);
-       }
-       for(int i=0; i<4; i++){
-           fdeck.add(10);
-       }
-     }
-    }
-    static void shuffleDeck(ArrayList<Integer> fdeck){
+    static void fillMatrix(int[][] arr, int left, int right) {
         Random gen = new Random();
-        for(int i=0; i< fdeck.size()-1; i++){
-            int randomIndex = gen.nextInt(fdeck.size()-(i+1))+(i+1);
-            int temp = fdeck.get(i);
-            fdeck.set(i,fdeck.get(randomIndex));
-            fdeck.set(randomIndex, temp);
+        for (int i = 0; i < arr.length; i++) {//arr.length перебирает количество строк в матрице
+            for (int j = 0; j < arr[i].length; j++) {//arr[i].length
+                arr[i][j] = gen.nextInt(right - left + 1) + left;
+            }
         }
     }
-    static void  playerTurn(ArrayList<Integer> fdeck, ArrayList<Integer> fplayer){
-        // игрок по желанию берет карты из колоды
-        Scanner in= new Scanner(System.in);
-        int answer = 1;
-        do {
-           playerTakeOneCard(fdeck, fplayer);
-            //подсчитать сколько у игрока очков и выдать результат
-            int playerScore = calcScore(fplayer);
-            System.out.println();
-            if(playerScore >=21) break;
-            else{
-                System.out.println();
-                answer = in.nextInt();
+
+    static void prinyMatrix(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j] + "");
             }
-            System.out.println("У вас " + playerScore + "очков");
-            if (playerScore >=21) break;
-            System.out.println("Хотите ли взять еще одну карту? 1 - да, любая цифра - нет");
-            answer = in.nextInt();
-        }while(answer == 1);
+            System.out.println();
+        }
     }
-    static void playerTakeOneCard(ArrayList<Integer> fdeck, ArrayList<Integer> fplayer){
-        //взять карту из колоды
-        int card = fdeck.get(fdeck.size() - 1);
-        //удалить карту из колоды
-        fdeck.remove(fdeck.size() - 1);
-        //добавить эту карту игроку
-        fplayer.add(card);
+
+    static void task1(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {//arr.length перебирает количество строк в матрице
+            for (int j = 0; j < arr[i].length; j++) {//arr[i].length
+                arr[i][j] = 10 * i;
+            }
+        }
     }
-    static int calcScore(ArrayList<Integer> fplayer){
+
+    static void task2(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {//arr.length перебирает количество строк в матрице
+            for (int j = 0; j < arr[i].length; j++) {//arr[i].length
+                arr[i][j] = 5 * i;
+            }
+        }
+    }
+
+    static void showString(int[][] arrayName, int k) {
+        if (k < 0 || k >= arrayName.length) {
+            System.out.println("Ошибка. Нет такой строки");
+        } else {
+            for (int i = 0; i < arrayName[k].length; i++) {
+                System.out.println(arrayName[k][i] + "");
+            }
+        }
+    }
+
+    static void showRow(int[][] arrayName, int k) {
+        if (k < 0 || k >= arrayName[0].length) {
+            System.out.println("Ошибка. Нет такого столбца");
+        } else {
+            for (int i = 0; i < arrayName.length; i++) {
+                System.out.println(arrayName[i][k] + "");
+            }
+        }
+    }
+
+    static void printEvenRows(int[][] arrayName) {
+        for (int i = 0; i < arrayName.length; i += 2) {
+            for (int j = 0; j < arrayName.length; j++) {
+                System.out.println(arrayName[i][j] + "");
+            }
+            System.out.println();
+        }
+    }
+
+    static int sumOfRow(int[][] arrayName, int k) {
         int sum = 0;
-        for(int i=0; i<fplayer.size(); i++) {
-            sum += fplayer.get(i);
+        if (k < 0 || k >= arrayName.length) {
+            System.out.println("");
+        } else {
+            for (int j = 0; j < arrayName[k].length; j++) {
+                sum += arrayName[k][j];
+            }
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        // создать новую колоду.
-        final int deckSize = 52;
-        ArrayList<Integer> deck = new ArrayList<>();
-        createDeck(deck);
-        // перетасовать колоду.
-        shuffleDeck(deck);
-        // создать игроков
-        ArrayList<Integer> player = new ArrayList<>();
-        // создать дилера
-        ArrayList<Integer> dealer = new ArrayList<>();
-        // ========действие игрока========
-        playerTurn(deck, player);
-        //проверка результатов действий игрока
-        //1. Игрок набрал 21 очко, игра заканчивается, игрок победил
-        if (calcScore(player) == 21){
-            System.out.println("Игрок выиграл. Конец игры. Поздравляем победителя");
-            return;
-        }
-        //2. Игрок набрал меньше 21 очка, игрок сказал хватит.
-        if (calcScore(player) < 21){
-            System.out.println("Игрок Проиграл. Конец игры. Удачи в следующий раз");
-            return;
-        }
-        //3. Игрок набрал больше 21 очка, игра заканчивается, игрок проиграл.
-        if (calcScore(player) > 21){
-            System.out.println("Игрок проиграл. Конец игры. Удачи в следующий раз");
-            return;
-        }
-        // ======== проверка результатов действий дилера========
-        //1. Дилер набрал 21 очко(у игрока заведомо <21) , игра заканчивается, Дилер победил
-        //2. Дилер набрал меньше 21 очка, игрок сказал хватит.
-        //3. Дилер набрал больше 21 очка, игра заканчивается, Дилер проиграл.
-        //3.1. У игрока больше очков, чем у дилера - выиграл игрок.
-        //3.2. У Дилера больше очков, чем у дилера - выиграл Дилер.
-        //3.3. У обоих поровну очков - ничья.
+        Scanner in = new Scanner(System.in);
+        final int n = 4;
+        final int m = 5;
+        int[][] mas = new int[n][m];
+        //int k;
+        //k= in.nextInt();
+        // fillMatrix(mas, 0, 10);
+        // prinyMatrix(mas);
 
+        // Matrix1. Даны целые положительные числа M и N. Сформировать целочисленную матрицу размера M × N, у которой все элементы I-й строки имеют
+        //значение 10·I (I = 1, . . ., M).
+        // task1(mas);
+        // prinyMatrix(mas);
+
+        //Matrix2. Даны целые положительные числа M и N. Сформировать целочисленную матрицу размера M × N, у которой все элементы J-го столбца
+        //имеют значение 5·J (J = 1, . . ., N).
+        // task2(mas);
+        // prinyMatrix(mas);
+
+        //Matrix7◦ Дана матрица размера M × N и целое число K (1 ≤ K ≤ M). Вывести
+        //элементы K-й строки данной матрицы.
+        // fillMatrix(mas, 0, 10);
+        // showString(mas, k);
+
+        //Matrix8. Дана матрица размера M × N и целое число K (1 ≤ K ≤ N). Вывести
+        //элементы K-го столбца данной матрицы.
+        // fillMatrix(mas, 0, 10);
+        // showString(mas, k);
+
+        //Matrix9. Дана матрица размера M × N. Вывести ее элементы, расположенные
+        //в строках с четными номерами (2, 4, . . .). Вывод элементов производить
+        //по строкам, условный оператор не использовать.
+        // fillMatrix(mas, -10, 10);
+        // prinyMatrix(mas);
+        // System.out.println("Строки с четными номерами");
+        // printEvenRows(mas);
+
+        //Matrix17. Дана матрица размера M × N и целое число K (1 ≤ K ≤ M). Найти
+        //сумму и произведение элементов K-й строки данной матрицы.
+        //fillMatrix(mas, 1, 5);
+        // prinyMatrix(mas);
+        // do {
+        //   System.out.println("Введите номер строки: ");
+        //   int k = in.nextInt();
+        //     System.out.println();
+        //  }while (k < 0 || k > mas.length);
+
+        //Сумма одной строки
+        // for(int i = 0; i< mas.length; i++)
+        // {
+        //   int k = 1;
+        //  int sum = 0;
+        //   for (int j = 0; j < mas[i].length; j++) {
+        //       sum += mas[i][j];
+
+        //    }
+        //    System.out.println(sum);
+
+            //Matrix 19. Дана матрица размера M × N. Для каждой строки матрицы найти
+            //сумму ее элементов.
+        //fillMatrix(mas, 1, 5 );
+        // prinyMatrix(mas);
+        // for(int i = 0; i < mas.length; i++)
+        //{
+        //    System.out.println("Сумма " +i + "-ой строки" + sumOfRow(mas,i));
+        // }
+
+        //
+        }
     }
-}
+
