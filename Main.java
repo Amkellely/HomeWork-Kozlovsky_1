feea424512c233b98000d38c8ac8ae170145c828
Amkellely <108361555+Amkellely@users.noreply.github.com>
2022-09-13 21:19:51 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,153 +1,165 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static void fillMatrix(int[][] arr, int left, int right) {
+
+    static void fillMatrix(int[][]  fmas){
         Random gen = new Random();
-        for (int i = 0; i < arr.length; i++) {//arr.length перебирает количество строк в матрице
-            for (int j = 0; j < arr[i].length; j++) {//arr[i].length
-                arr[i][j] = gen.nextInt(right - left + 1) + left;
+        for(int i = 0; i < fmas.length; i++){
+            for(int j = 0; j < fmas[i].length; j++){
+                fmas[i][j] = gen.nextInt(10);
             }
         }
     }
-
-    static void prinyMatrix(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.println(arr[i][j] + "");
-            }
-            System.out.println();
-        }
-    }
-
-    static void task1(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {//arr.length перебирает количество строк в матрице
-            for (int j = 0; j < arr[i].length; j++) {//arr[i].length
-                arr[i][j] = 10 * i;
+        static void task20(int[][]  fmas){
+            for(int k = 0; k < fmas[0].length; k++){
+            int p = 1 ;
+            for(int i = 0; i < fmas.length; i++){
+                p *= fmas[i][k];
+                 }
             }
         }
-    }
-
-    static void task2(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {//arr.length перебирает количество строк в матрице
-            for (int j = 0; j < arr[i].length; j++) {//arr[i].length
-                arr[i][j] = 5 * i;
-            }
-        }
-    }
-
-    static void showString(int[][] arrayName, int k) {
-        if (k < 0 || k >= arrayName.length) {
-            System.out.println("Ошибка. Нет такой строки");
-        } else {
-            for (int i = 0; i < arrayName[k].length; i++) {
-                System.out.println(arrayName[k][i] + "");
-            }
-        }
-    }
-
-    static void showRow(int[][] arrayName, int k) {
-        if (k < 0 || k >= arrayName[0].length) {
-            System.out.println("Ошибка. Нет такого столбца");
-        } else {
-            for (int i = 0; i < arrayName.length; i++) {
-                System.out.println(arrayName[i][k] + "");
-            }
-        }
-    }
-
-    static void printEvenRows(int[][] arrayName) {
-        for (int i = 0; i < arrayName.length; i += 2) {
-            for (int j = 0; j < arrayName.length; j++) {
-                System.out.println(arrayName[i][j] + "");
-            }
-            System.out.println();
-        }
-    }
-
-    static int sumOfRow(int[][] arrayName, int k) {
+        static int taskL1(int[][]  fmas){
         int sum = 0;
-        if (k < 0 || k >= arrayName.length) {
-            System.out.println("");
-        } else {
-            for (int j = 0; j < arrayName[k].length; j++) {
-                sum += arrayName[k][j];
+        for(int a = 0; a < fmas.length; a++){
+            sum +=fmas[a][a];
+        }
+            return sum;
+    }
+        static void initField(char[][] field){
+            for(int i = 0; i < field.length; i++){
+                for(int j = 0; j < field.length; j++){
+                    field[i][j] = ' ';
+                }
             }
         }
-        return sum;
-    }
+        static void printLineField(char[][] field){
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        }
+        static void printField(char[][] field){
+            for(int i = 0; i < field.length; i++){
+
+            }
+            for(int k = 0; k < 3; k++){
+                System.out.print("|---");
+            System.out.println('|');
+            for(int j = 0; j < field.length; j++){
+                System.out.println("|" + field[i][j]+ "");
+            }
+            for(int k = 0; k < 3; k++){
+                System.out.print("|---");
+            }
+            System.out.println('|');
+             }
+        }
+            static void turn(char[][] field, char symbol){
+                Scanner in = new Scanner(System.in);
+                int row = 0;
+                int column = 0;
+                do{
+                    System.out.println("Введите номер строки ");
+                    row = in.nextInt();
+                    System.out.println("Введите номер стобца ");
+                    column = in.nextInt();
+                }while(row < 0 || row > 2 || column < 0 || column > 2 || field[row][column] != ' ');
+                field[row][column] = symbol;
+            }
+            static boolean checkWin(char[][]field, char symbol){
+                boolean result = false;
+                //если в любой строке три одинаковых символа
+                for(int i = 0; i < field.length; i++){
+                    if(field[i][0] == field[i][1] && field[i][1] == field[i][2] && field[i][2] == symbol ) {
+                        result = true;
+                        break;
+                    }
+                }
+                //если в любом столбце три одинкаковых символа
+                for(int j = 0; j< field[0].length; j++){
+                    if(field[0][j] == field[1][j]&&field[1][j] == field[2][j]&&field[2][j] == field[3][j]&& field[3][j] == symbol){
+                        result = true;
+                        break;
+                    }
+                }
+                //если на главной диагонали три одинаковых символа
+                if(field[0][0] == field[1][1] && field[1][1] == field[2][2] && field[2][2] == symbol )
+                    result = true;
+
+                //если на побочной диагонали три одинаковых символа
+                else if(field[0][2] == field[1][1] && field[1][1] == field[2][0] && field[2][0] == symbol )
+                    result = true;
+                return result;
+             }
+    static boolean checkDraw(char[][]field){
+        boolean result = true;
+        for(int i = 0; i < field.length; i++){
+            for(int j = 0; j<field[i].length; j++){
+                if(field[i][j] == ' '){
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return  result;
+    }
+        public  void main(String[] args) {
+        // Matrix 20. Дана матрица размера M × N. Для каждого столбца матрицы найти
+        // произведение его элементов.
         final int n = 4;
         final int m = 5;
-        int[][] mas = new int[n][m];
-        //int k;
-        //k= in.nextInt();
-        // fillMatrix(mas, 0, 10);
-        // prinyMatrix(mas);
+        int[][] mas=new int [n][m];
+            fillMatrix(mas);
+            System.out.println(Arrays.deepToString(mas));
+            /*task20(mas);*/
+            final char symbolX = 'X';
+            final char symbolO = 'O';
+            final int size = 3;
+            //Найти сумму элементов главной диагонали матрицы
+            System.out.println(taskL1(mas));
 
-        // Matrix1. Даны целые положительные числа M и N. Сформировать целочисленную матрицу размера M × N, у которой все элементы I-й строки имеют
-        //значение 10·I (I = 1, . . ., M).
-        // task1(mas);
-        // prinyMatrix(mas);
-
-        //Matrix2. Даны целые положительные числа M и N. Сформировать целочисленную матрицу размера M × N, у которой все элементы J-го столбца
-        //имеют значение 5·J (J = 1, . . ., N).
-        // task2(mas);
-        // prinyMatrix(mas);
-
-        //Matrix7◦ Дана матрица размера M × N и целое число K (1 ≤ K ≤ M). Вывести
-        //элементы K-й строки данной матрицы.
-        // fillMatrix(mas, 0, 10);
-        // showString(mas, k);
-
-        //Matrix8. Дана матрица размера M × N и целое число K (1 ≤ K ≤ N). Вывести
-        //элементы K-го столбца данной матрицы.
-        // fillMatrix(mas, 0, 10);
-        // showString(mas, k);
-
-        //Matrix9. Дана матрица размера M × N. Вывести ее элементы, расположенные
-        //в строках с четными номерами (2, 4, . . .). Вывод элементов производить
-        //по строкам, условный оператор не использовать.
-        // fillMatrix(mas, -10, 10);
-        // prinyMatrix(mas);
-        // System.out.println("Строки с четными номерами");
-        // printEvenRows(mas);
-
-        //Matrix17. Дана матрица размера M × N и целое число K (1 ≤ K ≤ M). Найти
-        //сумму и произведение элементов K-й строки данной матрицы.
-        //fillMatrix(mas, 1, 5);
-        // prinyMatrix(mas);
-        // do {
-        //   System.out.println("Введите номер строки: ");
-        //   int k = in.nextInt();
-        //     System.out.println();
-        //  }while (k < 0 || k > mas.length);
-
-        //Сумма одной строки
-        // for(int i = 0; i< mas.length; i++)
-        // {
-        //   int k = 1;
-        //  int sum = 0;
-        //   for (int j = 0; j < mas[i].length; j++) {
-        //       sum += mas[i][j];
-
-        //    }
-        //    System.out.println(sum);
-
-            //Matrix 19. Дана матрица размера M × N. Для каждой строки матрицы найти
-            //сумму ее элементов.
-        //fillMatrix(mas, 1, 5 );
-        // prinyMatrix(mas);
-        // for(int i = 0; i < mas.length; i++)
-        //{
-        //    System.out.println("Сумма " +i + "-ой строки" + sumOfRow(mas,i));
-        // }
-
-        //
+            //Крестики нолики
+            final int size = 3;
+            char[][] field = new char [size][size];
+            //сделать пустое поле - заполнить его пробелами
+            initField (field);
+            printField(field);
+            //игровой цикл
+                while (true){
+                 //ход крестиков
+                    turn(field, 'x');
+                //запросить куда ставить крестик
+                //если координаты правильные - записать результат
+                //вывести координаты
+                    printField(field);
+            //если победа - конец игры - прервать цикл
+                    if(checkWin(field, 'X')){
+                        System.out.println("Выиграли крестики");
+                        break;
+                    }
+            //ничья - конец игры - прервать цикл
+                    if (checkDraw(field)) {
+                        System.out.println("Ничья");
+                        break;
+                    }
+            //ход ноликов
+                    turn(field, symbolO);
+                //запросить куда ставить нолик
+                //если координаты правильные - записать результат
+                //вывести координаты
+                    printField(field);
+            //победа - конец игры - прервать цикл
+                    if(checkWin(field, 'X')){
+                        System.out.println("Выиграли крестики");
+                        break;
+                    }
+            //ничья - конец игры - прервать цикл
+                    if (checkDraw(field)) {
+                        System.out.println("Ничья");
+                        break;
+                    }
+                }
         }
     }
 
